9364ca2b8012c73aa42724d4b17522727b45886b
PushyTao <58740747+PushyTao@users.noreply.github.com>
2022-01-22 17:06:36 +0800

    modified the main.go

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # booking-app
+
 learning go
+
+array 是固定长度的一种类型，而 slice 是内存大小动态开辟的一种类型
--- main.go
+++ main.go
@@ -3,21 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50 // const can't change the value of it
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50 // const can't change the value of it
+	var remainingTickets uint = 50
 	// %T可以用来输出类型
 	// fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We hava total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	//array
+	// var bookings [50]string //"tao", "tao2", "tao3"
+	// bookings[0] = "tao"     // the index is 0->n-1
+	// bookings[1] = "tao2"    // one by one
+	// slice
+	var bookings []string
+	var firstName string
+	var lastName string
+	var userTickets uint
+	var email string
 	// ask user's name
-	userName = "Tom"
-	userTickets = 2
+	fmt.Println("Please enter your first name: ")
+	fmt.Scan(&firstName) // 加上取地址符，否则不会输入
+	fmt.Println("Please enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Please enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Please enter number of tickets: ")
+	fmt.Scan(&userTickets)
 
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
-
+	remainingTickets = remainingTickets - userTickets
+	// bookings[0] = firstName + " " + lastName
+	// append添加一个元素到切片的末尾，并返回一个添加后的切片，显然可以直接赋给bookings
+	bookings = append(bookings, firstName+" "+lastName)
+	fmt.Printf("the whole array : %v\n", bookings)
+	fmt.Printf("The first value is %v\n", bookings[0])
+	fmt.Printf("Array type is %T\n", bookings)
+	fmt.Printf("Array length is :%v\n", len(bookings))
+	fmt.Printf("User %v %v booked %v tickets.\n", firstName, lastName, userTickets)
+	fmt.Printf("You will receive an confirmation email at %v\n", email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
